56396cb158789cb2ef18611841bf4d6924f507bd
Vincent <vrdphone@gmail.com>
2020-08-07 16:41:17 -0500

    Updated java script file to work

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,44 @@
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-
 let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numbers = "1234567890";
 let specialCharacters = "!#$&%&')(*+,-:;<=>?@[\]^_'{|}~";
-
-
-lowerCaseLetters = lowerCaseLetters.split("");
-upperCaseLetters = upperCaseLetters.split("");
-specialCharacters = specialCharacters.split("");
-numbers = numbers.split("");
-
+let charactersToUse = "";
+let passwordLength = 8;
 
 function generatePassword() {
-    let charactersToUse = [];
-    let passwordLength = passwordLengthPrompt();
-    let lowerCaseLog = confirm("Do you want to use lower case letters?\nOK=Yes\nCancel=No");
-    let upperCaseLog = confirm("Do you want to use upper case letters?\nOK=Yes\nCancel=No");
-    let numberLog = confirm("Do you want to use numbers?\nOK=Yes\nCancel=No");
-    let specialCharactersLog = confirm("Do you want to use special characters?\nOK=Yes\nCancel=No");
-    if (lowerCaseLog) {
-        charactersToUse.push(lowerCaseLetters);
-    }
-    if (upperCaseLog) {
-        charactersToUse.push(upperCaseLetters);
-    }
-    if (numberLog) {
-        charactersToUse.push(numbers);
-    }
-    if (specialCharactersLog) {
-        charactersToUse.push(specialCharacters);
-    }
-    console.log(charactersToUse);
+    charactersToUse = "";
+    let passwordCharacters = "";
+    passwordLength = prompt("Please pick a character length for your password.\nMinimum is 8 Characters\nMaximum is 128 Characters");
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        alert("Please enter a valid length selection");
+    } else {
+        let lowerCaseLog = confirm("Do you want to use lower case letters?\nOK=Yes\nCancel=No");
+        if (lowerCaseLog) {
+            charactersToUse = charactersToUse.concat(lowerCaseLetters);
+        }
+        let upperCaseLog = confirm("Do you want to use upper case letters?\nOK=Yes\nCancel=No");
+        if (upperCaseLog) {
+            charactersToUse = charactersToUse.concat(upperCaseLetters);
+        }
+        let numberLog = confirm("Do you want to use numbers?\nOK=Yes\nCancel=No");
+        if (numberLog) {
+            charactersToUse = charactersToUse.concat(numbers);
+        }
+        let specialCharactersLog = confirm("Do you want to use special characters?\nOK=Yes\nCancel=No");
+        if (specialCharactersLog) {
+            charactersToUse = charactersToUse.concat(specialCharacters);
+        }
 
-
+        for (var i = 0; i < passwordLength; i++) {
+            var randomCharacters = Math.floor(Math.random() * charactersToUse.length);
+            passwordCharacters = passwordCharacters + charactersToUse.charAt(randomCharacters);
+        }
+    }
+    return passwordCharacters;
 }
-
-function passwordLengthPrompt() {
-    let characterLenghtLog = prompt("Please pick a character length for your password.\nMinimum is 8 Characters\nMaximum is 128 Characters");
-    if (characterLenghtLog < 8 || characterLenghtLog > 128) {
-        return passwordLengthPrompt();
-    }
-    return characterLenghtLog;
-}
-
-
-
-
 
 function writePassword() {
     var password = generatePassword();
